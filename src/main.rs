b47620b655341b137adc10204edc2305a7d44864
jaq-cat <jaq.cat@protonmail.ch>
2020-11-17 15:39:38 -0300

    adding to bufr

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use std::fs;
 
 fn main() {
     //let args: Vec<_> = env::args().collect();
-    let mut buffer: Vec<Vec<char>> = Vec::new();
+    let mut buffer: Vec<Vec<c::chtype>> = vec![Vec::new()];
+    let mut lines: usize = 1;
     let mut ln: usize = 0;
     let mut cl: usize = 0;
     // initialize window
@@ -21,6 +22,18 @@ fn main() {
         if 32 < ch && ch < 127 {
             // valid ASCII
             c::addch(ch as c::chtype);
+            buffer[ln].push(ch as c::chtype);
+        } else {
+            match ch {
+                c::KEY_END => {
+                    ln += 1;
+                    if ln >= lines {
+                        buffer.push(Vec::new());
+                    }
+                    cl = buffer[ln].len();
+                }
+                _ => {}
+            }
         }
     }
     c::endwin();
