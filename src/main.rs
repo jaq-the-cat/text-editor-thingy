b32a0bf205f8cbefee1b03eafb020c63067b19fb
jaq-cat <jaq.cat@protonmail.ch>
2020-11-17 15:26:47 -0300

    init ncurses

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use ncurses as c;
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    //let args: Vec<_> = env::args().collect();
+    // initialize window
+    c::initscr();
+    let w = c::stdscr();
+    c::keypad(w, true);
+    c::noecho();
+    let mut maxy = 0;
+    let mut maxx = 0;
+    c::getmaxyx(w, &mut maxy, &mut maxx);
+    c::endwin();
 }
