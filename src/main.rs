0fdf76ae7ba5089b453b858146d1bd49b9db4bc9
jaq-cat <jaq.cat@protonmail.ch>
2020-11-17 15:36:28 -0300

    characters

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,24 @@ use std::fs;
 
 fn main() {
     //let args: Vec<_> = env::args().collect();
+    let mut buffer: Vec<Vec<char>> = Vec::new();
+    let mut ln: usize = 0;
+    let mut cl: usize = 0;
     // initialize window
     c::initscr();
     let w = c::stdscr();
     c::keypad(w, true);
     c::noecho();
-    let mut maxy = 0;
-    let mut maxx = 0;
-    c::getmaxyx(w, &mut maxy, &mut maxx);
+    let mut ch = 0;
+    loop {
+        let mut maxy = 0;
+        let mut maxx = 0;
+        c::getmaxyx(w, &mut maxy, &mut maxx);
+        ch = c::getch();
+        if 32 < ch && ch < 127 {
+            // valid ASCII
+            c::addch(ch as c::chtype);
+        }
+    }
     c::endwin();
 }
